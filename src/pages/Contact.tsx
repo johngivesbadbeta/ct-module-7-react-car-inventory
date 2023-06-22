662b76aec98983158ac988c3be520c7e114d289b
John Pinano <jpinano@gmail.com>
2023-06-22 11:28:23 -0700

    fixed error on contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1 className='underline text-black m-10 mb-3'> CONTACT </h1>
+      <p className ='ml-10 '>Info coming soon!</p>
+    </div>
+  )
+}
+
+export default Contact
